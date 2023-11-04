06fc8956eb6d3205d21a4e84acac60a4e64c3785
Sam <sams421@icloud.com>
2023-11-04 16:26:15 +0000

    Added boilerplate class validation functions

--- hamsterwallet/hamsterapp/views.py
+++ hamsterwallet/hamsterapp/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'mainpage.html')
+
+def validate_email(email): # -> bool
+    pass
+
+def hash_password(pw): # -> str
+    pass
+
+def validate_firstname(name): # -> bool
+    pass
+
+def validate_pricelimit(price): # -> bool
+    pass
+
+def validate_transactionname(name): # -> bool
+    pass
+
+def validate_category(name): # -> bool
+    pass
