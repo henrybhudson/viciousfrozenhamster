4dc69d3022a262c5231011d9478fafd9a8bf5384
Sam <sams421@icloud.com>
2023-11-04 16:17:33 +0000

    Added database models

--- hamsterwallet/hamsterapp/models.py
+++ hamsterwallet/hamsterapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    email = models.CharField(max_length=32,primary_key=True)
+    password = models.CharField(max_length=255)
+    firstName = models.CharField(max_length=32)
+    priceLimit = models.IntegerField()
+
+class categories(models.Model):
+    category = models.CharField(max_length=64)
+    priceLimit = models.IntegerField()
+
+class transactions(models.Model):
+    transactionID = models.IntegerField(primary_key=True)
+    email = models.ForeignKey(users, on_delete=models.CASCADE)
+    transactionName = models.CharField(max_length=255)
+    price = models.IntegerField()
+    category = models.ForeignKey(categories, on_delete=models.CASCADE)
+    date = models.DateTimeField()
