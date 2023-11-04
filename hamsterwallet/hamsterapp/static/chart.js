f10d77a5d4293f21b5e1d4e7702317bcb00d6967
Henry Hudson <henry.b.hudson@durham.ac.uk>
2023-11-04 23:00:03 +0000

    More Javscript

--- hamsterwallet/hamsterapp/static/chart.js
+++ hamsterwallet/hamsterapp/static/chart.js
@@ -1,5 +1,63 @@
 // Create the chart
+const COLOURS = {
+    "Bills": "#10451D",
+    "Charity": "#155D27",
+    "Food": "#1A7431",
+    "Entertainment": "#208B3A",
+    "Finances": "#25A244",
+    "General": "#2DC653",
+    "Groceries": "#4AD66D",
+    "Holidays": "#6EDE8A",
+    "Personal Care": "#92E6A7",
+    "Shopping": "#B7EFC5",
+    "Bank Transfers": "#C9F2D3",
+    "Transport": "#E8FFEE"
+}
+
+const CATEGORIES = ["Bills", "Charity", "Food", "Entertainment", "Finances", "General", "Groceries", "Holidays", "Personal Care", "Shopping", "Bank Transfers", "Transport"];
+
+
+const createData = (transactions) => {
+    let values = {};
+    let res = [];
+    CATEGORIES.forEach((category) => {
+        values[category] = 0
+    });
+
+    transactions.forEach((transaction) => {
+        values[transaction["category"]] += parseFloat(transaction["price"]);
+    });
+
+    for (const [category, value] of Object.entries(values)) {
+        res.push([category, value]);
+    }
+
+    return res;
+};
+
+
+// ```
+// Categorise the payment described "Tesco" using one of the following categories:
+// Bills
+// Charity
+// Food
+// Entertainment
+// Finances
+// General
+// Groceries
+// Holidays
+// Personal Care
+// Shopping
+// Bank Transfers
+// Transport
+// ```
+
+
+
+
 const f = () => {
+    const CIRCLE_SIZE = (window.innerWidth < 500) ? "120%" : "100%";
+
     Highcharts.chart('container', {
         chart: {
             renderTo: 'container',
@@ -17,10 +75,10 @@ const f = () => {
             }
         },
         series: [{
-            name: 'Browsers',
-            data: [["Firefox", 6], ["MSIE", 4], ["Chrome", 7]],
-            size: '100%',
-            innerSize: '70%',
+            name: 'Expenses',
+            data: [["Entertainment", 0.4], ["Personal Care", 1], ["Transport", 1.5], ["Food", 2], ["Groceries", 3], ["Available", 4]],
+            size: CIRCLE_SIZE,
+            innerSize: '80%',
             dataLabels: {
                 enabled: false
             }
