1819c5742ea6d09ba35b275c32f80d37b58a5db3
pawelliskiewicz <pawelliskiewicz@users.noreply.github.com>
2023-11-05 02:40:10 +0000

    slider added

--- hamsterwallet/hamsterapp/models.py
+++ hamsterwallet/hamsterapp/models.py
@@ -11,12 +11,10 @@ class users(models.Model):
     email = models.CharField(max_length=EMAIL_LENGTH,primary_key=True)
     password = models.CharField(max_length=PASSWORD_LENGTH)
     firstName = models.CharField(max_length=FIRSTNAME_LENGTH)
-    priceLimit = models.IntegerField()
 
 class categories(models.Model):
     category = models.CharField(max_length=CATEGORY_LENGTH,primary_key=True)
     email = models.ForeignKey(users, on_delete=models.CASCADE)
-    priceLimit = models.IntegerField()
 
 class transactions(models.Model):
     email = models.ForeignKey(users, on_delete=models.CASCADE)
